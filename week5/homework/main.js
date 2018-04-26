b426347ea92af8e5228524cfb34efa56915ada0c
Majid Khan <majidkhan@Majids-MacBook-Pro.local>
2018-04-26 22:19:17 +0200

    forgot to add week5 homework in first comming

---
+++ week5/homework/main.js
@@ -0,0 +1,26 @@
+let btn = document.getElementById("btn");
+let renderContainer = document.getElementById("json-response");
+
+btn.addEventListener('click', function() {
+    let ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET','https://api.github.com/orgs/HackYourFuture/repos');
+    ourRequest.onload = function() {
+        const ourData = JSON.parse(ourRequest.responseText);  /// responseText method return data as string. In order to console in browser it must be parsed.
+        renderData(ourData);
+    };
+    ourRequest.send();    
+});
+
+function renderData(data) {
+    let htmlString = "";
+    htmlString += "<table class='table table-striped'>";
+    htmlString += "<tr><th>Repository</th><th>Stargazers</th><th>Watchers</th><th>Forks</th><th>language</th></tr>";
+
+    for(let i = 0; i < data.length; i++) {
+        console.log(data);
+        htmlString += "<tr><td><a href='" + data[i].html_url + "' target='_blank'>" + data[i].name + "</a></td><td>" + data[i].stargazers_count + "</td><td>" + data[i].watchers + "</td><td>" + data[i].forks + "</td><td>" + data[i].language + "</td></tr>";
+    }
+    htmlString += "</table>";
+    renderContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
