d141f58932699260a46797112d0701b75c547b22
Majid Khan <majidkhan@Majids-MacBook-Pro.local>
2018-05-03 15:18:27 +0200

    adding arguments in a url request. loading data successfully and display in ul li

--- week6/homework/main.js
+++ week6/homework/main.js
@@ -1,11 +1,9 @@
-// Pankaj Review: "Code is to complex and is difficult to understand. Please make it simple."
-// Solution : Allready provided funtion is used i.e. fetchJSONData(). Tried to make it simple. The code down in the comment is left to show it to mentor that an alternate way to render data. It was taught in class. 
+
 let btn = document.getElementById("btn");
 
 function fetchJSONData(url, callbackFn) {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function() {
-        console.log("Data loaded.");
         const data = JSON.parse(xhr.responseText);
         callbackFn(data);
     });
@@ -18,29 +16,27 @@ function getRepoNames( data ) {
     const container = document.getElementById("items");
     let render_li = "";
     data.forEach( function(item)  {
-        render_li += `<li class="list-group-item"><a href="${item.html_url}" target="_blank">${item.name}</a></li>`;    // template literals to render data
-
-            /* #########     These commented lines have been replaced by the line of code above   
-            const element = document.createElement("li");
-            element.classList.add('list-group-item');
-            const link = document.createElement("a");
-            link.setAttribute('href',item.html_url);
-            link.setAttribute('target', '_blank');
-            const txtNode = document.createTextNode(item.name);
-            link.appendChild(txtNode);
-            element.appendChild(link);
-            container.appendChild(element);  
-            ############    */
+        render_li += `<li class="list-group-item"><a href="${item.html_url}" target="_blank">${item.name}</a></li>`; 
     });
     container.innerHTML = render_li;
     btn.innerHTML = "JSON data loaded";
 
 }
 
-
+function getSearchedData(searchData) {
+    let output = "";
+    for (let items in searchData) {
+        output += searchData.items;
+    }
+    return output; 
+}
 
 btn.addEventListener('click', function() {
-    fetchJSONData('https://api.github.com/orgs/HackYourFuture/repos', function(repoList) {
-        getRepoNames(repoList);
+    const elemInput = document.querySelector('.user-input');
+    let userInput = elemInput.value;
+    const searchQuery = 'https://api.github.com/search/repositories?q=user:HackYourFuture+' + userInput;
+    fetchJSONData(searchQuery, function(repoList) {
+        const searchedData = repoList.items.filter(getSearchedData);
+        getRepoNames(searchedData);
     });  
 });
